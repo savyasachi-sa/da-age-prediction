7980589d94225ac3211fc2e3391a0a2e77a73f33
astuti <asharma@eng.ucsd.edu>
2020-02-19 16:06:43 -0800

    add baseline resnet

---
+++ baseline_resnet.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+import torchvision.models as models
+
+class BaselineResnet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.resnet = models.resnet18(pretrained=True)
+        for param in self.resnet.parameters():
+            param.requires_grad = False #TODO: shouldn't involve fc layer??
+        self.resnet.fc = nn.Linear(512, 1, bias=True)
+
+    def forward(self, x):
+        print('in forward now')
+        self.resnet.forward(x)
