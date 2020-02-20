d0603c3d264dbaa6d8a2e41105a2f78040e52f03
Savyasachi <savyasachi.sa@gmail.com>
2020-02-19 16:28:21 -0800

    Add more FC layers in baseline.

--- baseline_resnet.py
+++ baseline_resnet.py
@@ -7,7 +7,11 @@ class BaselineResnet(nn.Module):
         self.resnet = models.resnet18(pretrained=True)
         for param in self.resnet.parameters():
             param.requires_grad = False #TODO: shouldn't involve fc layer??
-        self.resnet.fc = nn.Linear(512, 1, bias=True)
+        self.resnet.fc = nn.Sequential(
+            nn.Linear(512, 256, bias=True),
+            nn.Linear(256, 128, bias=True),
+            nn.Linear(128, 1, bias=True),
+        )
 
     def forward(self, x):
         print('in forward now')
