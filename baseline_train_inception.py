613dc6c54df087982f1a22ba06a381c4c5dbb3f4
agokhale <agokhale@agokhale-8722.pods.agokhale.svc.cluster.local>
2020-02-26 19:06:52 -0800

    inception training, plotting for val added

--- baseline_train_inception.py
+++ baseline_train_inception.py
@@ -1,8 +1,8 @@
-from dataset import UTK
+from dataset_inceptionV3 import UTK
 from dataset_design import DatasetDesign
 from nntools import Experiment
 from stats_manager import AgeStatsManager
-from baseline_resnet import BaselineResnet
+from baseline_inceptionv3 import BaselineInceptionV3
 from config import *
 import matplotlib.pyplot as plt
 import os
@@ -19,7 +19,6 @@ def report_loss(exp: Experiment, output_dir):
             exp.history[exp.epoch - 1][1]['accuracy']))
         plot_separate(exp, output_dir, plot_type='accuracy')
         plot_separate(exp, output_dir, plot_type='loss')
-
 
 # separately plot and save figures for accuracy and loss:
 def plot_separate(exp: Experiment, output_dir, plot_type='accuracy'):
@@ -44,7 +43,6 @@ def plot_separate(exp: Experiment, output_dir, plot_type='accuracy'):
         os.mkdir(plots_path)
     plt.savefig(plots_path + '/plot_validation_{}.png'.format(plot_type))
 
-
 ethnicity = {
     "source": 0,
     "target": 1,
@@ -57,13 +55,13 @@ train_dataset = UTK(SOURCE_TRAIN_PATH, random_flips=True)
 val_dataset = UTK(SOURCE_VAL_PATH, random_flips=False)
 
 # Setup Experiment
-net = BaselineResnet()
+net = BaselineInceptionV3()
 net = net.to(DEVICE)
 stats_manager = AgeStatsManager(WINDOW_THRESH)
 optimizer = torch.optim.Adam(net.parameters(), lr=BASELINE_CONFIG['learning_rate'])
-exp = Experiment(net, train_dataset, val_dataset, stats_manager, optimizer, BASELINE_CONFIG, output_dir="baseline",
+exp = Experiment(net, train_dataset, val_dataset, stats_manager, optimizer, BASELINE_CONFIG, output_dir="baseline_inceptionV3",
                  perform_validation_during_training=True)
 
 # Run Experiment
 
-exp.run(num_epochs=10, plot=lambda e: report_loss(e, output_dir='./baseline'))
+exp.run(num_epochs=250, plot=lambda e: report_loss(e, output_dir = './baseline_inceptionV3'))
---
+++ dataset_inceptionV3.py
@@ -0,0 +1,39 @@
+from torch.utils.data import Dataset
+from PIL import Image
+import pandas as pd
+import torchvision.transforms as transforms
+import torch
+
+
+class UTK(Dataset):
+    def __init__(self, csv_file, random_flips=True):
+        self.data = pd.read_csv(csv_file)
+        self.random_flips = random_flips
+        self.normalize = transforms.Compose([transforms.Resize(size = [299,299]),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        self.flip = transforms.Compose([
+            transforms.RandomHorizontalFlip()
+            # transforms.RandomVerticalFlip()
+        ])
+
+    def __len__(self):
+        return len(self.data)
+
+    def _get_item(self, idx):
+        img_name = self.data.iloc[idx, 0]
+        img_full = Image.open(img_name).convert('RGB')
+        label_age = self.data.iloc[idx, 1]
+
+        if self.random_flips:
+            self.flip(img_full)
+
+        img_full = self.normalize(img_full)
+
+        label_age = torch.tensor(label_age).type(torch.FloatTensor)
+        return img_full, label_age
+
+    def __getitem__(self, idx):
+        return self._get_item(idx)
