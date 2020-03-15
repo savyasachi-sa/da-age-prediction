2d8ef7fb6acd6ec5241b84b3a097ec4b47ac51dd
astuti <asharma@eng.ucsd.edu>
2020-03-15 16:03:21 -0700

    update and test exp statistics

--- experiment_statistics.py
+++ experiment_statistics.py
@@ -3,24 +3,41 @@ import os
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
-from config import * #USE Config only for datasets
-from dataset import UTK
+from config import *  # USE Config only for datasets
+from dataset_factory import *
 import torch.utils.data as td
 
+from final_resnet import FinalResnet
 from stats_manager import *
+
+
+def plot_save(output_dir, epochs, plot_name, x_name, y_s={}):
+    plt.clf()
+    plt.xlabel(x_name)
+    x = [i + 1 for i in range(epochs)]
+    for key in y_s:
+        plt.plot(x, y_s[key], label = key)
+    plt.legend()
+    plots_path = os.path.join(output_dir, "")
+
+    if not os.path.exists(plots_path):
+        os.mkdir(plots_path)
+    plt.savefig(plots_path + '/{}.png'.format(plot_name))
+
 
 class GenStatsManager(object):
 
     def __init__(self):
-        self.init()
+        self.init(None, [])
 
     def __repr__(self):
         return self.__class__.__name__
 
-    def init(self, metrics_names = [], win_thresh=3):
+    def init(self, file_path, metrics_names=[], win_thresh=3):
         self.number_update = 0
         self.win_thresh = win_thresh
         self.metrics = dict()
+        self.file_path = file_path
         for key in metrics_names:
             self.metrics[key] = 0
 
@@ -31,30 +48,36 @@ class GenStatsManager(object):
         if 'accuracy' in self.metrics:
             metric_instance['accuracy'] = torch.mean((torch.abs(y - d) <= self.win_thresh).float())
         self._accumulate(metric_instance)
-        return  metric_instance
+        return metric_instance
 
-    def _accumulate(self, metrics= {}):
-        for key, val in metrics:
-            self.metrics[key] += val
+    def _accumulate(self, metrics={}):
+        for key in metrics:
+            self.metrics[key] += metrics[key]
         self.number_update += 1
 
-    def summarize(self):
-        for key, val in self.metrics:
+    def summarize(self, save=True):
+        for key in self.metrics:
             self.metrics[key] /= self.number_update
+        if (save):
+            self._save_file()
         return self.metrics
+
+    def _save_file(self):
+        with open(self.file_path, 'w') as f:
+            f.write(str(self.metrics))
+
 
 class ExperimentStatistics():
 
     def __init__(self, network, output_dir, checkpoint_path, is_adaptive=False):
         self.checkpoint = torch.load(checkpoint_path, None)
         self.output_dir = output_dir
-        self.batch_size = 16 # TODO: any would work?
+        self.batch_size = 16  # TODO: any would work?
         self.is_adaptive = is_adaptive
         self.network = network
-        self.parent_stats = self.checkpoint['Stats'] #TODO: check key
         self.train_losses = []
-        self.val_losses = [] #only one value if no val values were stored
-        self.target_loss = [] #will not be calculate for each iterations
+        self.val_losses = []  # only one value if no val values were stored
+        self.target_loss = []  # will not be calculate for each iterations
         self.stats_manager = GenStatsManager()
 
     def optimizer_stats(self):
@@ -71,78 +94,81 @@ class ExperimentStatistics():
                 f.write(str(content) + '\n')
 
     def parent_epoch_losses(self):
-        parent_stats = self.checkpoint['stats']
-        self.parent_train = np.array(parent_stats)[:,0]
-        self.parent_val = np.array(parent_stats)[:,1]
-        self.parent_target = np.array(parent_stats)[:,2]
-        #TODO: plot and save
+        if (self.is_adaptive):
+            try:
+                parent_stats = self.checkpoint['Stats']
+                all_stats = {}
+                all_stats['train'] = np.array(parent_stats)[:, 0]
+                all_stats['val'] = np.array(parent_stats)[:, 1]
+                all_stats['target'] = np.array(parent_stats)[:, 2]
+                plot_save(self.output_dir, len(parent_stats), 'all_parent_stats', 'epochs', all_stats)
+            except BaseException as e:
+                print('An exception occurred while ready parent epoch losses: {}'.format(e))
 
-
-    def network_predictions(self, metric_names = ['loss']):
+    def network_predictions(self, metric_names=['loss']):
         self.get_data_loader()
         self.network.load_state_dict(self.checkpoint['Net'])
         self.network.eval()
-        self.stats_manager.init()
         with torch.no_grad():
-            self.stats_manager.init(metric_names)
+            self.stats_manager.init('{}train_model_stats.txt'.format(self.output_dir), metric_names)
             for x, d in self.train_loader:
                 x, d = x.to(self.network.device), d.to(self.network.device)
                 d = d.view([len(d), 1])
                 y = self.network.forward(x)
                 loss = self.network.criterion(y, d)
                 self.stats_manager.calc_store(loss, x, y, d)
+                self.stats_manager.summarize()
 
-            self.stats_manager.init(metric_names)
+            self.stats_manager.init('{}val_model_stats.txt'.format(self.output_dir), metric_names)
             for x, d in self.val_loader:
                 x, d = x.to(self.network.device), d.to(self.network.device)
                 d = d.view([len(d), 1])
                 y = self.network.forward(x)
                 loss = self.network.criterion(y, d)
                 self.stats_manager.calc_store(loss, x, y, d)
+                self.stats_manager.summarize()
 
-            self.stats_manager.init(metric_names)
-            if(self.is_adaptive):
+            self.stats_manager.init('{}target_model_stats.txt'.format(self.output_dir), metric_names)
+            if (self.is_adaptive):
                 for x, d in self.target_loader:
                     x, d = x.to(self.network.device), d.to(self.network.device)
                     d = d.view([len(d), 1])
                     y = self.network.forward(x)
                     loss = self.network.criterion(y, d)
                     self.stats_manager.calc_store(loss, x, y, d)
-
-
+                    self.stats_manager.summarize()
 
     def iteration_stats(self):
         history = self.checkpoint['History']
-        if (history==None or len(history) == 0):  #TODO: throw error
+        losses = {}
+        if (history == None or len(history) == 0):  # TODO: throw error
             return
         if (isinstance(history[0], tuple)):  # checks if has validation scores
-            self.train_losses = np.array(history)[:, 0] #TODO: check history if np array
-            self.val_losses = np.array(history)[:, 1] #TODO: in text file?
-            #TODO: plot both
+            losses['train_losses'] = np.array(history)[:, 0]  # TODO: check history if np array
+            losses['val_losses'] = np.array(history)[:, 1]  # TODO: in text file?
+            plot_save(self.output_dir, len(history), 'iteration_losses', 'iteration', losses)
+        else:
+            losses['train_losses'] = np.array(history)
+            plot_save(self.output_dir, len(history), 'iteration_losses_only_train', 'iteration', losses)
 
     def get_data_loader(self):
-        #TODO: get val and target data loader
-        train_set = UTK(SOURCE_TRAIN_PATH, random_flips=False)
-        val_set = UTK(SOURCE_VAL_PATH, random_flips=False)
-        target_set = UTK(TARGET_TRAIN_PATH, random_flips=False)
+        # TODO: get val and target data loader
+        train_set, val_set, target_set = get_datasets()
         self.train_loader = td.DataLoader(train_set, batch_size=self.batch_size, shuffle=False,
-                                        pin_memory=True)
+                                          pin_memory=True)
         self.val_loader = td.DataLoader(val_set, batch_size=self.batch_size, shuffle=False,
                                         pin_memory=True)
         self.target_loader = td.DataLoader(target_set, batch_size=self.batch_size, shuffle=True,
                                            pin_memory=True)
 
 
-    def plot_save(self, epochs, name, y_s ={}):
-        plt.clf()
-        plt.xlabel('Epoch')
-        x = [i+1 for i in range(epochs)]
-        for key,y in y_s:
-            plt.ylabel(key.upper())
-            plt.plot(x, y[key])
-        plt.legend()
-        plots_path = os.path.join(self.output_dir, "plots")
 
-        if not os.path.exists(plots_path):
-            os.mkdir(plots_path)
-        plt.savefig(plots_path + '/{}.png'.format(name))
+
+
+# Test
+# net = FinalResnet()
+# exp = ExperimentStatistics(net, './results/', './models/astuti_test1_Adaptive_L1/checkpoint.pth.tar', is_adaptive=True)
+# exp.optimizer_stats()
+# exp.parent_epoch_losses()
+#exp.iteration_stats()
+#exp.network_predictions()
